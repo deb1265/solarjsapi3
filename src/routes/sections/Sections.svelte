28446e3fd045459ab83f208105ea8081e0875aa4
deb1265 <117177026+deb1265@users.noreply.github.com>
2024-03-28 16:42:06 -0400

    Update Sections.svelte

--- src/routes/sections/Sections.svelte
+++ src/routes/sections/Sections.svelte
@@ -1,18 +1,4 @@
-<!--
- Copyright 2023 Google LLC
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- -->
 
 <script lang="ts">
   /* global google */
@@ -21,7 +7,6 @@
   import { findSolarConfig } from '../utils';
   import BuildingInsightsSection from './BuildingInsightsSection.svelte';
   import DataLayersSection from './DataLayersSection.svelte';
-  import SolarPotentialSection from './SolarPotentialSection.svelte';
 
   export let location: google.maps.LatLng;
   export let map: google.maps.Map;
@@ -36,9 +21,9 @@
 
   // User settings
   let monthlyAverageEnergyBillInput = 300;
-  let panelCapacityWattsInput = 250;
+  let panelCapacityWattsInput = 400;
   let energyCostPerKwhInput = 0.31;
-  let dcToAcDerateInput = 0.85;
+  let dcToAcDerateInput = 1.35;
 
   // Find the config that covers the yearly energy consumption.
   let yearlyKwhEnergyConsumption: number;
@@ -81,18 +66,6 @@
       {buildingInsights}
       {geometryLibrary}
       {map}
-    />
-
-    <md-divider inset />
-    <SolarPotentialSection
-      bind:expandedSection
-      bind:configId
-      bind:monthlyAverageEnergyBillInput
-      bind:energyCostPerKwhInput
-      bind:panelCapacityWattsInput
-      bind:dcToAcDerateInput
-      solarPanelConfigs={buildingInsights.solarPotential.solarPanelConfigs}
-      defaultPanelCapacityWatts={buildingInsights.solarPotential.panelCapacityWatts}
     />
   {/if}
 </div>
