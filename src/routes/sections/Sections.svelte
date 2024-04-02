33b63f262a05cc35402e300923403d92672ec692
deb1265 <117177026+deb1265@users.noreply.github.com>
2024-04-02 17:29:31 -0400

    Update Sections.svelte

--- src/routes/sections/Sections.svelte
+++ src/routes/sections/Sections.svelte
@@ -1,34 +1,38 @@
-
-
 <script lang="ts">
+  // Global declaration for google object
   /* global google */
 
+  // Importing necessary components and types
   import type { BuildingInsightsResponse } from '../solar';
   import { findSolarConfig } from '../utils';
   import BuildingInsightsSection from './BuildingInsightsSection.svelte';
   import DataLayersSection from './DataLayersSection.svelte';
+  import SolarPotentialSection from './SolarPotentialSection.svelte';
 
+  // Exporting props to be used in other components
   export let location: google.maps.LatLng;
   export let map: google.maps.Map;
   export let geometryLibrary: google.maps.GeometryLibrary;
   export let googleMapsApiKey: string;
-
+  export let initialMonthlyAverageEnergyBillInput: number;
+  // Local state for building insights, initially undefined
   let buildingInsights: BuildingInsightsResponse | undefined;
 
-  // State
+  // Local state for UI control
   let expandedSection: string = '';
   let showPanels = true;
 
-  // User settings
-  let monthlyAverageEnergyBillInput = 300;
+  // User input states
+  let monthlyAverageEnergyBillInput = initialMonthlyAverageEnergyBillInput;
   let panelCapacityWattsInput = 400;
   let energyCostPerKwhInput = 0.31;
-  let dcToAcDerateInput = 1.35;
+  let dcToAcDerateInput = 0.85;
 
-  // Find the config that covers the yearly energy consumption.
+  // Calculating yearly energy consumption based on user input
   let yearlyKwhEnergyConsumption: number;
   $: yearlyKwhEnergyConsumption = (monthlyAverageEnergyBillInput / energyCostPerKwhInput) * 12;
 
+  // Finding the appropriate solar config based on the yearly consumption
   let configId: number | undefined;
   $: if (configId === undefined && buildingInsights) {
     const defaultPanelCapacity = buildingInsights.solarPotential.panelCapacityWatts;
@@ -43,6 +47,7 @@
 </script>
 
 <div class="flex flex-col rounded-md shadow-md">
+
   {#if geometryLibrary && map}
     <BuildingInsightsSection
       bind:expandedSection
@@ -57,15 +62,19 @@
     />
   {/if}
 
+
   {#if buildingInsights && configId !== undefined}
     <md-divider inset />
-    <DataLayersSection
+    <SolarPotentialSection
       bind:expandedSection
-      bind:showPanels
-      {googleMapsApiKey}
-      {buildingInsights}
-      {geometryLibrary}
-      {map}
+      bind:configId
+      bind:monthlyAverageEnergyBillInput
+      bind:energyCostPerKwhInput
+      bind:panelCapacityWattsInput
+      bind:dcToAcDerateInput
+      solarPanelConfigs={buildingInsights.solarPotential.solarPanelConfigs}
+      defaultPanelCapacityWatts={buildingInsights.solarPotential.panelCapacityWatts}
     />
   {/if}
 </div>
+
