5fbbf41c82774fb126aaf4786533b5ef47a46d26
deb1265 <117177026+deb1265@users.noreply.github.com>
2024-04-06 15:31:29 -0400

    Update DataLayersSection.svelte

--- src/routes/sections/DataLayersSection.svelte
+++ src/routes/sections/DataLayersSection.svelte
@@ -61,7 +61,7 @@
   let dataLayersResponse: DataLayersResponse | undefined;
   let requestError: RequestError | undefined;
   let apiResponseDialog: MdDialog;
-  let layerId: LayerId | 'none' = 'none';
+  let layerId: LayerId | 'rgb' = 'rgb';
   let layer: Layer | undefined;
   let imageryQuality: 'HIGH' | 'MEDIUM' | 'LOW';
 
